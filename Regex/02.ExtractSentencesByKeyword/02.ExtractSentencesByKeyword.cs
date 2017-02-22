34cb2505dff2c154c3dbe185d3ba7591d9dd2f4b
NikiSpasov <niki.spasow@gmail.com>
2017-02-22 17:58:20 +0200

    some files

---
+++ Regex/02.ExtractSentencesByKeyword/02.ExtractSentencesByKeyword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var keyWord = Console.ReadLine();
+        keyWord = " " + keyWord + " ";
+        var text = Console.ReadLine()
+            .Split(new char[] { '.', '!', '?' },
+            StringSplitOptions.RemoveEmptyEntries);
+        Regex regex = new Regex(keyWord);
+        foreach (var item in text)
+        {
+            if (regex.IsMatch(item))
+            {
+                Console.WriteLine(item.Trim());
+            }
+        }
+    }
+}
---
+++ Regex/03.ExctractUsernames/03.ExctractUsernames.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        var text = Console.ReadLine()
+            .Split(new char[] { '/', '\\', '(', ')', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int maxCnt = 0;
+        string longestWord = string.Empty;
+        foreach (var user in text)
+        {
+            int lenght = user.Length;
+            if (lenght<maxCnt)
+            {
+                maxCnt = lenght;
+                longestWord = user;
+            }
+
+        }
+        Console.WriteLine(longestWord);
+
+        
+        //var matches = regex.Matches(text);
+        //foreach (Match users in matches)
+        //{
+          //  Console.WriteLine(users);
+        //}
+
+    }
+}
