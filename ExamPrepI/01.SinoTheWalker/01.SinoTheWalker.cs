b19660a8c33ea2f1d0096dfbf02b5086a14a42ba
Niki <niki.spasow@gmail.com>
2017-02-22 15:37:26 +0200

    Ädded some exam tasks

---
+++ ExamPrepI/01.SinoTheWalker/01.SinoTheWalker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var startTime = Console.ReadLine();
+        var steps = int.Parse(Console.ReadLine());
+        var secondsForAStep = double.Parse(Console.ReadLine());
+        BigInteger totalSeconds = steps * (int)secondsForAStep;
+        DateTime start = DateTime.ParseExact(startTime, "HH:mm:ss", CultureInfo.InvariantCulture);
+        DateTime end = start.AddSeconds((double)totalSeconds);
+        Console.WriteLine("Time Arrival: {0:HH:mm:ss}", end);
+    }
+}
+
+
+//80 ot 100
---
+++ ExamPrepI/02.SoftUniKaraoke/02.SoftUniKaraoke.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+class Program
+{
+    private static List<Singer> ListSingers()
+    {
+        var singers = Console.ReadLine()
+            .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+        var songs = Console.ReadLine()
+            .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => x.Trim())
+            .ToList();
+
+        var input = Console.ReadLine();
+
+        List<Singer> singersList = new List<Singer>();
+        while (input != "dawn")
+        {
+            List<string> performance = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToList();
+
+            Singer currentSinger = new Singer();
+
+            currentSinger.Name = performance[0];
+            currentSinger.Song = performance[1];
+            string award = performance[2];
+            currentSinger.Awards = new List<string>();
+            currentSinger.Awards.Add(award);
+
+            if (singers.Contains(currentSinger.Name) && songs.Contains(currentSinger.Song))
+            {
+                singersList.Add(currentSinger);
+            }  
+
+            input = Console.ReadLine();
+
+        }
+        return singersList;
+
+    }
+    static void Main()
+    {
+
+        List<Singer> singers = ListSingers();
+
+    }
+
+}
+//        if (singersList.Count == 0)
+//        {
+//            Console.WriteLine("No awards");
+//        }
+//        else
+//        {
+
+//        }
+//    }
+//}
