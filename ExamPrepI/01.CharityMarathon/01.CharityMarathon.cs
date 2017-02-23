50f00812d089c5f12ccfc641866aff05627b1fae
Niki <niki.spasow@gmail.com>
2017-02-23 12:40:39 +0200

    some exam prep

---
+++ ExamPrepI/01.CharityMarathon/01.CharityMarathon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        var daysOfMarathon = int.Parse(Console.ReadLine());
+        var numberOfRuners = int.Parse(Console.ReadLine());
+        var avgLapsEveryRunnerMakes = int.Parse(Console.ReadLine());
+        var trackLength = int.Parse(Console.ReadLine());
+        var trackCapacity = int.Parse(Console.ReadLine());
+        var moneyPerKilometer = double.Parse(Console.ReadLine());
+
+        var totalRunners = 0;
+        var totalRunnersPossible = trackCapacity * daysOfMarathon;
+        if (numberOfRuners >= totalRunnersPossible)
+        {
+            totalRunners = totalRunnersPossible;
+        }
+        else
+        {
+            totalRunners = numberOfRuners;
+        }
+
+        ulong totalMeters = (ulong)(totalRunners * avgLapsEveryRunnerMakes * trackLength);
+        ulong totalKilometers = totalMeters / 1000;
+        decimal totalMoney = (totalKilometers * (decimal)moneyPerKilometer);
+        Console.WriteLine($"Money raised: {totalMoney:0.00}");
+    }
+}
--- ExamPrepI/03.Endurance Rally/03.Endurance Rally.cs
+++ ExamPrepI/03.Endurance Rally/03.Endurance Rally.cs
@@ -10,6 +10,49 @@ namespace _03.Endurance_Rally
     {
         static void Main(string[] args)
         {
+            var driversNames = Console.ReadLine()
+                .Split(' ');
+
+            var trackLayouts = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            var checkPointIndexes = Console.ReadLine()
+              .Split(' ')
+              .Select(int.Parse)
+              .ToArray();
+
+            foreach (var driver in driversNames)
+            {
+                var driverNameSplitted = driver.ToCharArray();
+                double initialFuel = driverNameSplitted[0];
+                for (int i = 0; i < trackLayouts.Length; i++)
+                {
+                    if (checkPointIndexes.Contains(i))
+                    {
+                        initialFuel += trackLayouts[i];
+                    }
+                    else
+                    {
+                        initialFuel -= trackLayouts[i];
+                    }
+                    if (initialFuel > 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{driver} - reached {i}");
+                        break;
+                    }
+                }
+                if (initialFuel > 0)
+                {
+                    Console.WriteLine("{0} - fuel left {1:f2}", driver, initialFuel);
+                }
+            }
+
         }
     }
 }
